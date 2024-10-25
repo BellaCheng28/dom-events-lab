e2f4d51a12d295c8ec4b73526bb4649f9b884563
User Name <xiaojiecheng@Xiaojies-MacBook-Pro.local>
2024-10-25 16:29:49 -0400

    add modified items.

--- app.js
+++ app.js
@@ -1,67 +1,89 @@
-/*-------------------------------- Constants and Variables --------------------------------*/
+// User stories
+// AAU I should see 0 on my display when I open the calc
+// AAU I should be able to click on the numbers and see them in the display, numbers get concat on the display
+// AAU I should be able to apply operators to my calculation
+// AAU I should be able to input another numerical value to finish a chosen operation(same thing ad 1st stroy)
+// AAU I should be able to execute the operation by clicking = and the value on display should be the output of the calculation
+// AAU I should be able to clear the calculator at any given time and display shows 0
 
-let currentInput = '';
-let previousInput = '';
-let operator = '';
+// Pseudocode
+// Step 1: When user clicks a number display it to the screen
+//         If they click another number it should add on to the display
+//         Store the number if no operator is clicked as numOne
+// Step 2: If a user clicks an operator
+//         Add operator to the display
+//         If another operator is clicked, replace the existing one
+// Step 3: If the user clicks another number start adding that to the display
+//         Store value after operator as numTwo
+// Step 4: If the user clicks = then calculate the numOne + operator + numTwo
+//         Display the result
+// Step 5: If a user clicks clear at any time, clear the display as well as numOne and numTwo
 
-/*------------------------ Cached Element References ------------------------*/
-const numberButtons = document.querySelectorAll('.button.number');
-const display = document.querySelector('.display');
-const operatorButtons = document.querySelectorAll('.button.operator');
-const equalsButton = document.querySelector('.button.equals');
-const clearButton = document.querySelector('.button.operator.C');
+// Variables
+let numOne = "";
+let numTwo = "";
+let operator = "";
+let calcOutput = "";
 
-/*----------------------------- Event Listeners -----------------------------*/
-numberButtons.forEach(button => {
-  button.addEventListener('click', function() {
-    currentInput += this.innerText; // Append number
-    display.innerText = currentInput; // Update display
+// Cached Elements (NOT CONSTANTS)
+const clearBtn = document.querySelector(".clear");
+const display = document.querySelector(".display");
+const equalsBtn = document.querySelector(".equals");
+const numberBtns = document.querySelectorAll(".number");
+const operatorBtns = document.querySelectorAll(".operator");
+
+// Functions
+const clearClick = (event) => {
+  display.innerText = ''
+  numOne = ''
+  numTwo = ''
+  operator = ''
+  calcOutput = ''
+
+  operatorBtns.forEach((btn) => {
+    btn.style.border = "";
   });
-});
+}
 
-operatorButtons.forEach(opButton => {
-  opButton.addEventListener('click', function() {
-    if (currentInput) {
-      previousInput = currentInput; // Store current input
-      operator = this.innerText; // Store operator
-      currentInput = ''; // Reset current input for next number
-    }
-  });
-});
-
-// Equals button event listener
-equalsButton.addEventListener('click', function() {
-  if (previousInput && currentInput) {
-    let result;
-    const prev = parseFloat(previousInput);
-    const curr = parseFloat(currentInput);
-    
-    switch (operator) {
-      case '+':
-        result = prev + curr;
-        break;
-      case '-':
-        result = prev - curr;
-        break;
-      case '*':
-        result = prev * curr;
-        break;
-      case '/':
-        result = prev / curr;
-        break;
-    }
-    
-    display.innerText = result; // Show result on display
-    currentInput = result; // Update current input for further calculations
-    previousInput = ''; // Reset previous input
-    operator = ''; // Reset operator
+const numClick = (event) => {
+  if (calcOutput) {
+    clearClick()
   }
+
+  if (numOne && operator) {
+    display.innerText += event.target.innerText;
+    numTwo = display.innerText;
+  } else {
+    display.innerText += event.target.innerText;
+    numOne = display.innerText;
+  }
+};
+
+const operatorClick = (event) => {
+  const clickedOperator = event.target.innerText;
+  operator = clickedOperator;
+
+  operatorBtns.forEach((btn) => {
+    btn.style.border = "";
+  });
+  event.target.style.border = "2px white solid";
+  display.innerText = "";
+};
+
+const equalsClick = (event) => {
+  // Take numOne and numTwo and calculate them, based on operator
+  calcOutput = eval(numOne + operator + numTwo)
+  display.innerText = calcOutput
+}
+
+// Event listeners
+clearBtn.addEventListener('click', clearClick)
+equalsBtn.addEventListener('click', equalsClick)
+
+numberBtns.forEach((btn) => {
+  btn.addEventListener("click", numClick);
 });
 
-// Clear button event listener
-clearButton.addEventListener('click', function() {
-  currentInput = '';
-  previousInput = '';
-  operator = '';
-  display.innerText = ''; // Reset display
+operatorBtns.forEach((btn) => {
+  btn.addEventListener("click", operatorClick);
 });
